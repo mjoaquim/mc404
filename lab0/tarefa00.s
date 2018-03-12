918ad50663cbd7dc33f4d976c98f1382ffcd3ec2
Mateus Joaquim <mateusjoaquim@estudiovh.com.br>
2018-03-12 19:53:23 -0300

    Comentarios lab 00

--- lab0/tarefa00.s
+++ lab0/tarefa00.s
@@ -1,8 +1,10 @@
 @ Mateus Joaquim RA: 184083
-@ Lab 1 Orientacoes:
-@ Escreva um programa para calcular o valor da expressão
-@ 4*(0x5000 + 0x200)
-@ usando apenas os registradores r0 e r1, e deixando o resultado no registrador r0. O seu programa deve ser montado a partir do endereço 0x200.
+@ Lab 0 Orientacoes:
+@ Reserve espaço para três variáveis inteiras na memória, presentes respectivamente nos endereços de rótulo varA, varB e varC. Os endereços de varA, varB e varC devem ser respectivamente 0x100, 0x104 e 0x108. (Utilize a diretivas .skip ou .word para reservar espaço para as variáveis)
+@ Coloque no registrador r0 o valor da soma das três variáveis MAIS o valor constante 0x1000.
+@ Para testar, você pode inicializar as variáveis com valores conhecidos. Note no entanto que no Susy os valores das variáveis serão diferentes a cada teste (o sistema vai alterar o valor das variáveis antes da execução de sua solução).
+
+
 
 @ Definir constantes
 constante .equ 0x1000
@@ -15,12 +17,12 @@ varC:   .skip   0x108
 
 .org 0x200
 
-start:               @ um rótulo padrão, vamos usar no Susy
-      set r0, varA         @ Seta
-      set r1, varB
-      set r2, varC
-      set r3, constante
-      add r0,r1
-      add r0,r2
-      add r0,r3
-      hlt                     @ a execução
+start:                     @ um rótulo padrão, vamos usar no Susy
+      set r0, varA         @ Seta r0 com varA
+      set r1, varB         @ Seta r1 com varB
+      set r2, varC         @ Seta r2 com varC
+      set r3, constante    @ seta r3 com a constante
+      add r0,r1            @ adiciona r1 e r0 e armazena em r0
+      add r0,r2            @ adiciona r0 e r2 e armazena em r0
+      add r0,r3            @ adiciona r0 e r3 e armazena em r0
+      hlt                  @ termina a execução
