24dfd22e8d0163592db6f33ef4d584276ecac7bb
Mateus Joaquim <mateusjoaquim@estudiovh.com.br>
2018-03-27 21:50:30 -0300

    leitura de 16bits

--- lab02/tarefa02.s
+++ lab02/tarefa02.s
@@ -5,9 +5,8 @@
 .org 0x100
 @ Reserva memoria para variaveis.
 divisor:          .skip     4                    @ Caractere procurado
-num_elementos:    .word      0                    @ contador de posicao
-flag:             .word      0                    @ flag para verificacao se achou o caractere em todo o vetor ou não
-
+num_elementos:    .word      0                   @ contador de posicao
+soma:             .word      0
 .org 0x200
 vetor:            .skip     400                      @ Rotulo p/ o susy submeter a sequencia
 
@@ -15,19 +14,20 @@ vetor:            .skip     400                      @ Rotulo p/ o susy submeter
 
 inicio:
       set r1, 0x200	                          @Colocando o endereço apontado do primeiro registrador no r1
-      ld r2, vetor                         	@Colocando o endereço apontado do segundo registrador no r2, para ir incrementando
+      ld  r2, vetor                         	@Colocando o endereço apontado do segundo registrador no r2, para ir incrementando
+      ld  r3,vetor
       ld  r4, divisor                         @ seta cada variavel em cada registrador
       ld  r5, num_elementos
+      ld  r0, soma
 
 prox_dig:
       cmp r5, 0	                             @Compara com 0 para verificar se é o último termo
       jz  final_for                     @ Precisamos fazer uma correcao de contagem a mais, estamos contando a partir do 1, e não da posicao zero de vetor
-      set r0, 0x0f
-      and r2,r0
+      shr r2,4*4
+      shl r3,4*4
+      shr r3,4*4
       sub r5,1                               @incrementa contador
-      jmp prox_dig                           @ volta o loop
-
-
+      jmp prox_dig
 
 
 final_for:
